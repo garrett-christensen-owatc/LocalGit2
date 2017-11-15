19612fac57a418b1b5f01a0fc0e4d005d827e9f9
garrett.christensen <garrett.christensen@BT158-05>
2017-11-15 11:10:49 -0700

    Initial Commit

--- LocalGit/Form1.Designer.cs
+++ LocalGit/Form1.Designer.cs
@@ -28,25 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnhello = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btnhello
+            // button1
             // 
-            this.btnhello.Location = new System.Drawing.Point(12, 21);
-            this.btnhello.Name = "btnhello";
-            this.btnhello.Size = new System.Drawing.Size(75, 23);
-            this.btnhello.TabIndex = 0;
-            this.btnhello.Text = "hello";
-            this.btnhello.UseVisualStyleBackColor = true;
-            this.btnhello.Click += new System.EventHandler(this.btnhello_Click);
+            this.button1.Location = new System.Drawing.Point(61, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "hello";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.btnhello);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -55,7 +54,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnhello;
+        private System.Windows.Forms.Button button1;
     }
 }
 
